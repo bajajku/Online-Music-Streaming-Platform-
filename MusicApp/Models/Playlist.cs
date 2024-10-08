51af8f63f8596a6ca3484617fdc693ad760a3b9d
bajajku <bajajku@sheridancollege.ca>
2024-10-08 19:14:02 -0400

    Created Models for User, Song, and Playlist

---
+++ MusicApp/Models/Playlist.cs
@@ -0,0 +1,7 @@
+public class Playlist
+{
+    public int PlaylistId { get; set; }
+    public string Name { get; set; }
+    public User User { get; set; }
+    public ICollection<Song> Songs { get; set; }
+}
---
+++ MusicApp/Models/Song.cs
@@ -0,0 +1,9 @@
+public class Song
+{
+    public int SongId { get; set; }
+    public string Title { get; set; }
+    public string Artist { get; set; }
+    public Album Album { get; set; }
+    public TimeSpan Duration { get; set; }
+    public string FileLocation { get; set; }
+}

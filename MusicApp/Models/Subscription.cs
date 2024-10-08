2db4c446ce132f85625c1b1a1aee0a05777dbdd5
bajajku <bajajku@sheridancollege.ca>
2024-10-08 19:01:00 -0400

    Created dto models for Album and subsription classes

---
+++ MusicApp/Models/Subscription.cs
@@ -0,0 +1,8 @@
+public class Subscription
+{
+    public int SubscriptionId { get; set; }
+    public string Tier { get; set; }
+    public bool AdFree { get; set; }
+    public bool HighQualityAudio { get; set; }
+    public bool OfflineAccess { get; set; }
+}

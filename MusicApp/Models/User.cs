e6b3481bb5d6c064805d39a73cd0b09ff83fab63
Zhaoyi Song <songzha@sheridancollege.ca>
2024-10-08 22:38:05 -0400

    Branch initial commit.

--- MusicApp/Models/User.cs
+++ MusicApp/Models/User.cs
@@ -1,9 +1,9 @@
 public class User
 {
-    public int UserId { get; set; }
+    public Guid Id { get; set; }
     public string Username { get; set; }
     public string Password { get; set; }
     public string Email { get; set; }
     public bool IsPremium { get; set; }
-    public ICollection<Playlist> Playlists { get; set; }
+    public ICollection<Playlist>? Playlists { get; set; }
 }

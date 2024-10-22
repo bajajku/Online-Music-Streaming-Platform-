876cd93ea747964700122ebfb1f79c3c248e5104
Zhaoyi Song <songzha@sheridancollege.ca>
2024-10-21 22:08:59 -0400

    Added Registration and Login Dtos.

---
+++ MusicApp/Models/Dtos/LoginDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicApp.Models.Dtos
+{
+    public class LoginDto
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        [MaxLength(20, ErrorMessage = "Max 20 chararcters allowed.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "Max 20 or min 5 chararcters allowed.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}

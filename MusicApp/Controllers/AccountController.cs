dfef16c78972475ec2f3be8dead1890a5859c719
Neha Desai <desaineh@shernet.sheridancollege.ca>
2024-10-23 17:16:09 -0400

    created many views for the UI and updated the controllers

---
+++ MusicApp/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MusicApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            // Add logic for user authentication
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Signup(User user)
+        {
+            // Add logic to save the user to the database
+            return RedirectToAction("Login");
+        }
+
+        public IActionResult Profile()
+        {
+            return View(); // Future profile page
+        }
+    }
+}

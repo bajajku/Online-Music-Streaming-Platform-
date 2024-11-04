9cac64ef084bab4fe6feb996e86d93c1785fcdd6
Neha Desai <desaineh@shernet.sheridancollege.ca>
2024-11-04 11:30:33 -0500

    Playlist Page created

---
+++ MusicApp/Controllers/PlaylistController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace MusicApp.Controllers
+{
+    public class PlaylistController : Controller
+    {
+        public IActionResult Index()
+        {
+            var playlists = new List<string> { "Playlist 1", "Playlist 2", "Playlist 3", "Playlist 4" };
+            var model = new
+            {
+                Username = "SampleUser",
+                UserID = "12345",
+                Playlists = playlists
+            };
+            return View(model);
+        }
+    }
+}

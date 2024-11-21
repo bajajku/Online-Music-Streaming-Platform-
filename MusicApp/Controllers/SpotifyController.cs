72f3b4e13de656d90d82b2b8f02279f0319907c6
bajajku <bajajku@sheridancollege.ca>
2024-11-21 00:18:27 -0500

    Added Swagger UI, tested some basic endpoints, will exand the endpoints now.

--- MusicApp/Controllers/SpotifyController.cs
+++ MusicApp/Controllers/SpotifyController.cs
@@ -2,8 +2,11 @@ using Microsoft.AspNetCore.Mvc;
 using SpotifyMVC.Services;
 using System.Threading.Tasks;
 
+
 namespace SpotifyMVC.Controllers
 {
+    [ApiController]
+    [Route("api")]
     public class SpotifyController : Controller
     {
         private readonly ISpotifyService _spotifyService;
@@ -13,44 +16,50 @@ namespace SpotifyMVC.Controllers
             _spotifyService = spotifyService;
         }
 
+        [HttpGet("index")]
         public async Task<IActionResult> Index()
         {
             var featuredPlaylists = await _spotifyService.GetFeaturedPlaylistsAsync();
             var newReleases = await _spotifyService.GetNewReleasesAsync();
-            return View((featuredPlaylists, newReleases));
+            return Ok(new { featuredPlaylists, newReleases });
         }
 
+        [HttpGet("search")]
         public async Task<IActionResult> Search(string query)
         {
             if (string.IsNullOrEmpty(query))
-                return View(new List<string>());
+                return Ok(new List<string>());
 
             var tracks = await _spotifyService.SearchTracksAsync(query);
-            return View(tracks);
+            return Ok(tracks);
         }
 
+        [HttpGet("track")]
         public async Task<IActionResult> Track(string id)
         {
             var track = await _spotifyService.GetTrackAsync(id);
-            return View(track);
+            return Ok(track);
         }
 
+        [HttpGet("artist/top-tracks")]
         public async Task<IActionResult> ArtistTopTracks(string id)
         {
             var tracks = await _spotifyService.GetArtistTopTracksAsync(id);
-            return View(tracks);
+            return Ok(tracks);
         }
 
+        [HttpGet("playlist")]
         public async Task<IActionResult> PlaylistTracks(string id)
         {
             var tracks = await _spotifyService.GetPlaylistTracksAsync(id);
-            return View(tracks);
+            return Ok(tracks);
         }
 
+        [HttpGet("categories")]
         public async Task<IActionResult> Categories()
         {
             var categories = await _spotifyService.GetCategoriesAsync();
-            return View(categories);
+            return Ok(categories);
         }
     }
 }

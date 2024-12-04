f5c1df1a4af0314877a451e043743e9e1f1c8211
bajajku <bajajku@sheridancollege.ca>
2024-12-04 00:20:19 -0500

    Refactor PlaylistController to enhance playlist details structure and update ViewPlaylist to display tracks with improved data representation

--- MusicApp/Controllers/PlaylistController.cs
+++ MusicApp/Controllers/PlaylistController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SpotifyMVC.Models;
 
 public class PlaylistController : Controller
 {
@@ -18,13 +19,57 @@ public class PlaylistController : Controller
         // Simulate fetching the playlist details by ID
         var playlist = new Playlist
         {
-            PlaylistId = id,
-            Name = $"Playlist {id}",
-            Songs = new List<Song>
-        {
-            new Song { SongId = 1, Title = "Song 1", Artist = "Artist 1", Duration = TimeSpan.FromMinutes(3) },
-            new Song { SongId = 2, Title = "Song 2", Artist = "Artist 2", Duration = TimeSpan.FromMinutes(4) }
-        }
+            Id = id.ToString(),
+            Name = "My Playlist",
+            Description = "A collection of my favorite songs",
+            Images = new List<SpotifyImage>
+            {
+                new SpotifyImage { Url = "https://via.placeholder.com/150" }
+            },
+            ExternalUrls = new ExternalUrls { Spotify = "https://open.spotify.com/playlist/123" },
+            Owner = new PlaylistOwner
+            {
+                Id = "user123",
+                DisplayName = "Sample User",
+                ExternalUrls = new ExternalUrls { Spotify = "https://open.spotify.com/user/user123" }
+            },
+            Public = true,
+            Tracks = new PlaylistTracks
+            {
+                Total = 10,
+                Items = new List<PlaylistTrack>
+                {
+                    new PlaylistTrack
+                    {
+                        Track = new Track
+                        {
+                            Id = "track123",
+                            Name = "Song 1",
+                            Artists = new List<Artist> { new Artist { Name = "Artist 1" } },
+                            Album = new Album { Name = "Album 1" },
+                            DurationMs = 180000,
+                            ExternalUrls = new ExternalUrls { Spotify = "https://open.spotify.com/track/track123" },
+                            PreviewUrl = "https://p.scdn.co/mp3-preview/preview.mp3",
+                            IsPlayable = true
+                        }
+                    },
+                    new PlaylistTrack
+                    {
+                        Track = new Track
+                        {
+                            Id = "track456",
+                            Name = "Song 2",
+                            Artists = new List<Artist> { new Artist { Name = "Artist 2" } },
+                            Album = new Album { Name = "Album 2" },
+                            DurationMs = 200000,
+                            ExternalUrls = new ExternalUrls { Spotify = "https://open.spotify.com/track/track456" },
+                            PreviewUrl = "https://p.scdn.co/mp3-preview/preview.mp3",
+                            IsPlayable = true
+                        }
+                    }
+                }
+            }
+            
         };
 
         return View(playlist); // Pass the playlist to the view
